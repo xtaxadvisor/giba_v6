651ee1d1ac5a910eab07b93c0a47349484062dd2
Gilberto <gdoffice@mail.com>
2025-06-07 11:49:42 -0400

    fix: Remove console.log definition from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,6 @@ export default defineConfig({
     }
   },
   define: {
-    'process.env.NODE_ENV': '"production"',
-    'console.log': '()=>{}'
+    'process.env.NODE_ENV': JSON.stringify('production')
   }
 });
